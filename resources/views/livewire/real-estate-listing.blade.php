51a1a18a37aad7c32222a78ca8586558f5ea8449
cuong pham <cuongdtnt109@gmail.com>
2026-01-13 10:40:38 +0700

    update code

--- resources/views/livewire/real-estate-listing.blade.php
+++ resources/views/livewire/real-estate-listing.blade.php
@@ -229,7 +229,7 @@
                         <div class="md:col-span-3">
                             <label class="block text-sm font-bold text-gray-700 mb-1">Nhu cầu <span
                                     class="text-red-500">*</span></label>
-                            <select wire:model="type"
+                            <select wire:model.live="type"
                                 class="w-full border border-gray-300 rounded-lg px-4 py-2.5 bg-white focus:ring-2 focus:ring-blue-500 outline-none shadow-sm">
                                 <option>Cần bán</option>
                                 <option>Cho thuê</option>
